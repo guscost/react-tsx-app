9d76155f0c6e8c6f9e4705bc7cc5a993a65f672e
Gus Cost <guscost@gmail.com>
2025-01-29 23:18:56 -0500

    Reorganize files

--- www/js/index.js
+++ www/js/index.js
@@ -3,4 +3,4 @@ define(["require", "exports", "react/jsx-runtime", "react-dom", "Hero"], functio
     Object.defineProperty(exports, "__esModule", { value: true });
     (0, react_dom_1.createRoot)(document.getElementById("root")).render((0, jsx_runtime_1.jsxs)("div", { children: [(0, jsx_runtime_1.jsx)(Hero_1.default, {}), (0, jsx_runtime_1.jsx)("br", {}), (0, jsx_runtime_1.jsx)("em", { children: "Have a nice day!" })] }));
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50c3giXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0lBR0EsSUFBQSxzQkFBVSxFQUFDLFFBQVEsQ0FBQyxjQUFjLENBQUMsTUFBTSxDQUFDLENBQUMsQ0FBQyxNQUFNLENBQ2hELDRDQUNFLHVCQUFDLGNBQUksS0FBRyxFQUNSLGdDQUFNLEVBQ04sOERBQXlCLElBQ3JCLENBQ1AsQ0FBQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHN4Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztJQUdBLElBQUEsc0JBQVUsRUFBQyxRQUFRLENBQUMsY0FBYyxDQUFDLE1BQU0sQ0FBQyxDQUFDLENBQUMsTUFBTSxDQUNoRCw0Q0FDRSx1QkFBQyxjQUFJLEtBQUcsRUFDUixnQ0FBTSxFQUNOLDhEQUF5QixJQUNyQixDQUNQLENBQUMifQ==
