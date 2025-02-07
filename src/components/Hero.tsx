45e752adcad72b94242edb977937f5001bc3986d
Gus Cost <guscost@gmail.com>
2025-02-06 23:47:33 -0500

    Theme support and GitHub link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,40 @@
-import { useState } from "react";
+import { useTheme } from "next-themes";
+import { Github, Moon, Sun } from "lucide-react";
+import { Button } from "./ui/button";
 
 export function Hero() {
-  const [count, setCount] = useState(0);
+  const { theme, setTheme } = useTheme();
 
   return (
-    <div className="mb-8">
-      <h1 className="text-2xl">React TSX App</h1>
-      <p>
-        A bundle-free React app scaffolding kit with shadcn/ui and all its
-        dependencies ready to go.
-      </p>
+    <div className="flex justify-between mb-8">
+      <div>
+        <h1 className="text-2xl">React TSX App</h1>
+        <p>
+          A bundle-free app kit that includes{" "}
+          <a className="underline" href="https://ui.shadcn.com">
+            shadcn/ui
+          </a>{" "}
+          and its dependencies.
+        </p>
+      </div>
+      <div className="flex gap-1">
+        <a href="https://github.com/guscost/react-tsx-app" target="_blank">
+          <Button variant="ghost" className="w-10">
+            <Github fill="currentColor" />
+          </Button>
+        </a>
+        <Button
+          variant="ghost"
+          className="w-10"
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+          {theme === "light" ? (
+            <Moon fill="currentColor" />
+          ) : (
+            <Sun fill="currentColor" />
+          )}
+        </Button>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom";
+import { ThemeProvider } from "next-themes";
 
 import { CardsDemo } from "./components/demo-cards/index";
 import { Hero } from "./components/hero";
 
 createRoot(document.getElementById("root")).render(
-  <div className="p-4">
-    <Hero />
-    <CardsDemo />
-  </div>,
+  <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <div className="p-8">
+      <Hero />
+      <CardsDemo />
+    </div>
+  </ThemeProvider>,
 );
